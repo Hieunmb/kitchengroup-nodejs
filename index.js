97a7b5067f11c1c8c17640ef37ec55cca640f3b0
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2022-12-12 15:35:47 +0700

    commit 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app = express();
-const PORT= process.env.PORT || 5001;
+const PORT= process.env.PORT || 3505;
 const mysql = require("mysql");
 
 app.listen(PORT,function (){
